4881e69d55fad0228fd14066eab1ad1a7cdb341b
jbettini <ashhxtm@gmail.com>
2024-08-24 04:09:01 +0200

    fix scripts blfs

--- Part-III/install_blfs.sh
+++ Part-III/install_blfs.sh
@@ -351,6 +351,57 @@ EOF
     rm -fv /etc/pam.d/other
     make install &&
     chmod -v 4755 /usr/sbin/unix_chkpwd
+
+install -vdm755 /etc/pam.d &&
+cat > /etc/pam.d/system-account << "EOF" &&
+# Begin /etc/pam.d/system-account
+
+account   required    pam_unix.so
+
+# End /etc/pam.d/system-account
+EOF
+
+cat > /etc/pam.d/system-auth << "EOF" &&
+# Begin /etc/pam.d/system-auth
+
+auth      required    pam_unix.so
+
+# End /etc/pam.d/system-auth
+EOF
+
+cat > /etc/pam.d/system-session << "EOF" &&
+# Begin /etc/pam.d/system-session
+
+session   required    pam_unix.so
+
+# End /etc/pam.d/system-session
+EOF
+
+cat > /etc/pam.d/system-password << "EOF"
+# Begin /etc/pam.d/system-password
+
+# use yescrypt hash for encryption, use shadow, and try to use any
+# previously defined authentication token (chosen password) set by any
+# prior module.
+password  required    pam_unix.so       yescrypt shadow try_first_pass
+
+# End /etc/pam.d/system-password
+EOF
+
+cat > /etc/pam.d/other << "EOF"
+# Begin /etc/pam.d/other
+
+auth        required        pam_warn.so
+auth        required        pam_deny.so
+account     required        pam_warn.so
+account     required        pam_deny.so
+password    required        pam_warn.so
+password    required        pam_deny.so
+session     required        pam_warn.so
+session     required        pam_deny.so
+
+# End /etc/pam.d/other
+EOF
 popd
 
 rm -rf Linux-PAM-1.6.0
@@ -361,37 +412,163 @@ rm -rf Linux-PAM-1.6.0
 tar xvf shadow-4.14.5.tar.xz
 
 pushd shadow-4.14.5
-    sed -i 's/groups$(EXEEXT) //' src/Makefile.in
-    find man -name Makefile.in -exec sed -i 's/groups\.1 / /'   {} \;
-    find man -name Makefile.in -exec sed -i 's/getspnam\.3 / /' {} \;
-    find man -name Makefile.in -exec sed -i 's/passwd\.5 / /'   {} \;
+    sed -i 's@DICTPATH.*@DICTPATH\t/lib/cracklib/pw_dict@' etc/login.defs
+    sed -i 's/groups$(EXEEXT) //' src/Makefile.in          &&
+    find man -name Makefile.in -exec sed -i 's/groups\.1 / /'   {} \; &&
+    find man -name Makefile.in -exec sed -i 's/getspnam\.3 / /' {} \; &&
+    find man -name Makefile.in -exec sed -i 's/passwd\.5 / /'   {} \; &&
 
-    sed -e 's:#ENCRYPT_METHOD DES:ENCRYPT_METHOD YESCRYPT:' \
-    -e 's:/var/spool/mail:/var/mail:'                   \
-    -e '/PATH=/{s@/sbin:@@;s@/bin:@@}'                  \
-    -i etc/login.defs
+    sed -e 's@#ENCRYPT_METHOD DES@ENCRYPT_METHOD YESCRYPT@' \
+        -e 's@/var/spool/mail@/var/mail@'                   \
+        -e '/PATH=/{s@/sbin:@@;s@/bin:@@}'                  \
+        -i etc/login.defs                                   &&
 
-    touch /usr/bin/passwd
     ./configure --sysconfdir=/etc   \
                 --disable-static    \
-                --with-{b,yes}crypt \
                 --without-libbsd    \
-                --with-group-name-max-length=32
-    
-    make exec_prefix=/usr install
-    make -C man install-man
+                --with-{b,yes}crypt &&
+    make
+    make exec_prefix=/usr pamddir= install
+    install -v -m644 /etc/login.defs /etc/login.defs.orig &&
+    for FUNCTION in FAIL_DELAY               \
+                    FAILLOG_ENAB             \
+                    LASTLOG_ENAB             \
+                    MAIL_CHECK_ENAB          \
+                    OBSCURE_CHECKS_ENAB      \
+                    PORTTIME_CHECKS_ENAB     \
+                    QUOTAS_ENAB              \
+                    CONSOLE MOTD_FILE        \
+                    FTMP_FILE NOLOGINS_FILE  \
+                    ENV_HZ PASS_MIN_LEN      \
+                    SU_WHEEL_ONLY            \
+                    CRACKLIB_DICTPATH        \
+                    PASS_CHANGE_TRIES        \
+                    PASS_ALWAYS_WARN         \
+                    CHFN_AUTH ENCRYPT_METHOD \
+                    ENVIRON_FILE
+    do
+        sed -i "s/^${FUNCTION}/# &/" /etc/login.defs
+    done
+cat > /etc/pam.d/login << "EOF"
+# Begin /etc/pam.d/login
 
-    pwconv
-    grpconv
+# Set failure delay before next prompt to 3 seconds
+auth      optional    pam_faildelay.so  delay=3000000
 
-    mkdir -p /etc/default
-    useradd -D --gid 999
-    sed -i '/MAIL/s/yes/no/' /etc/default/useradd
-cat << EOF > /tmp/rootpasswd
-root:root
+# Check to make sure that the user is allowed to login
+auth      requisite   pam_nologin.so
+
+# Check to make sure that root is allowed to login
+# Disabled by default. You will need to create /etc/securetty
+# file for this module to function. See man 5 securetty.
+#auth      required    pam_securetty.so
+
+# Additional group memberships - disabled by default
+#auth      optional    pam_group.so
+
+# include system auth settings
+auth      include     system-auth
+
+# check access for the user
+account   required    pam_access.so
+
+# include system account settings
+account   include     system-account
+
+# Set default environment variables for the user
+session   required    pam_env.so
+
+# Set resource limits for the user
+session   required    pam_limits.so
+
+# Display the message of the day - Disabled by default
+#session   optional    pam_motd.so
+
+# Check user's mail - Disabled by default
+#session   optional    pam_mail.so      standard quiet
+
+# include system session and password settings
+session   include     system-session
+password  include     system-password
+
+# End /etc/pam.d/login
 EOF
-    chpasswd < /tmp/rootpasswd
-    rm /tmp/rootpasswd
+
+cat > /etc/pam.d/passwd << "EOF"
+# Begin /etc/pam.d/passwd
+
+password  include     system-password
+
+# End /etc/pam.d/passwd
+EOF
+
+cat > /etc/pam.d/su << "EOF"
+# Begin /etc/pam.d/su
+
+# always allow root
+auth      sufficient  pam_rootok.so
+
+# Allow users in the wheel group to execute su without a password
+# disabled by default
+#auth      sufficient  pam_wheel.so trust use_uid
+
+# include system auth settings
+auth      include     system-auth
+
+# limit su to users in the wheel group
+# disabled by default
+#auth      required    pam_wheel.so use_uid
+
+# include system account settings
+account   include     system-account
+
+# Set default environment variables for the service user
+session   required    pam_env.so
+
+# include system session settings
+session   include     system-session
+
+# End /etc/pam.d/su
+EOF
+
+cat > /etc/pam.d/chpasswd << "EOF"
+# Begin /etc/pam.d/chpasswd
+
+# always allow root
+auth      sufficient  pam_rootok.so
+
+# include system auth and account settings
+auth      include     system-auth
+account   include     system-account
+password  include     system-password
+
+# End /etc/pam.d/chpasswd
+EOF
+
+sed -e s/chpasswd/newusers/ /etc/pam.d/chpasswd >/etc/pam.d/newusers
+
+cat > /etc/pam.d/chage << "EOF"
+# Begin /etc/pam.d/chage
+
+# always allow root
+auth      sufficient  pam_rootok.so
+
+# include system auth and account settings
+auth      include     system-auth
+account   include     system-account
+
+# End /etc/pam.d/chage
+EOF
+
+for PROGRAM in chfn chgpasswd chsh groupadd groupdel \
+               groupmems groupmod useradd userdel usermod
+do
+    install -v -m644 /etc/pam.d/chage /etc/pam.d/${PROGRAM}
+    sed -i "s/chage/$PROGRAM/" /etc/pam.d/${PROGRAM}
+done
+
+if [ -f /etc/login.access ]; then mv -v /etc/login.access{,.NOUSE}; fi
+if [ -f /etc/limits ]; then mv -v /etc/limits{,.NOUSE}; fi
 
 popd
 
@@ -429,9 +606,6 @@ pushd systemd-255
             ..
         ninja
         ninja install
-        tar -xf ../../systemd-man-pages-255.tar.xz \
-            --no-same-owner --strip-components=1   \
-            -C /usr/share/man
         systemd-machine-id-setup
         systemctl preset-all
     popd
@@ -504,7 +678,7 @@ rm -rf openssh-9.6p1
 ################LMDB_0.9.31.tar.gz#################
 tar xvf LMDB_0.9.31.tar.gz
 
-pushd LMDB_0.9.31
+pushd lmdb-LMDB_0.9.31
     cd libraries/liblmdb &&
     make                 &&
     sed -i 's| liblmdb.a||' Makefile
@@ -651,22 +825,7 @@ tar xvf libassuan-2.5.6.tar.bz2
 pushd libassuan-2.5.6
     ./configure --prefix=/usr &&
     make                      &&
-
-    make -C doc html                                                       &&
-    makeinfo --html --no-split -o doc/assuan_nochunks.html doc/assuan.texi &&
-    makeinfo --plaintext       -o doc/assuan.txt           doc/assuan.texi
-    make -C doc pdf ps
-    make install &&
-
-    install -v -dm755   /usr/share/doc/libassuan-2.5.6/html &&
-    install -v -m644 doc/assuan.html/* \
-                        /usr/share/doc/libassuan-2.5.6/html &&
-    install -v -m644 doc/assuan_nochunks.html \
-                        /usr/share/doc/libassuan-2.5.6      &&
-    install -v -m644 doc/assuan.{txt,texi} \
-                        /usr/share/doc/libassuan-2.5.6
-    install -v -m644  doc/assuan.{pdf,ps,dvi} \
-                    /usr/share/doc/libassuan-2.5.6
+    make install 
 popd
 
 rm -rf libassuan-2.5.6
@@ -691,22 +850,9 @@ pushd gnupg-2.4.4
 
     ../configure --prefix=/usr           \
                 --localstatedir=/var    \
-                --sysconfdir=/etc       \
-                --docdir=/usr/share/doc/gnupg-2.4.4 &&
-    make &&
-
-    makeinfo --html --no-split -I doc -o doc/gnupg_nochunks.html ../doc/gnupg.texi &&
-    makeinfo --plaintext       -I doc -o doc/gnupg.txt           ../doc/gnupg.texi &&
-    make -C doc html
-    make install &&
-
-    install -v -m755 -d /usr/share/doc/gnupg-2.4.4/html            &&
-    install -v -m644    doc/gnupg_nochunks.html \
-                        /usr/share/doc/gnupg-2.4.4/html/gnupg.html &&
-    install -v -m644    ../doc/*.texi doc/gnupg.txt \
-                        /usr/share/doc/gnupg-2.4.4 &&
-    install -v -m644    doc/gnupg.html/* \
-                        /usr/share/doc/gnupg-2.4.4/html
+                --sysconfdir=/etc &&
+    make 
+    make install
 popd
 
 rm -rf gnupg-2.4.4
@@ -727,7 +873,7 @@ rm -rf git-2.44.0
 ################icu4c-74_2-src.tgz#################
 tar xvf icu4c-74_2-src.tgz
 
-pushd icu4c-74_2-src
+pushd icu
     cd source                                    &&
 
     ./configure --prefix=/usr                    &&
@@ -735,7 +881,7 @@ pushd icu4c-74_2-src
     make install
 popd
 
-rm -rf icu4c-74_2-src
+rm -rf icu
 
 ################libxml2-2.12.5.tar.xz#################
 tar xvf libxml2-2.12.5.tar.xz
